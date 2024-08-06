3ee915449f46352c6ed80c77c9b9d88e7f7320a5
Mustafa <mus19pro@gmail.com>
2024-08-06 14:21:52 +0300

    Fixed drawer

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,11 +3,10 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 import Drawer from './Drawer';
 
-const ImageButton = ({ src, alt, onClick }) => (
+const ImageButton = ({ src, alt, onClick, isClose = false }) => (
     <button
         onClick={onClick}
-        className="hover:bg-gray-100 rounded-full transition-colors"
-    >
+        className={`rounded-full transition-colors ${isClose ? '' : 'hover:bg-gray-100'}`}>
         <Image src={src} alt={alt} width={32} height={32} />
     </button>
 );
@@ -25,11 +24,12 @@ export function NavBar() {
 
     return (
         <>
-            <nav className="flex items-center justify-between px-4 py-2 bg-white fixed top-0 left-0 right-0 z-50">
+            <nav className="flex items-center justify-between p-5 bg-white absolute top-0 left-0 right-0 z-50">
                 <ImageButton
                     src={isDrawerOpen ? "/close.svg" : "/menu.svg"}
                     alt={isDrawerOpen ? "Close Menu" : "Open Menu"}
                     onClick={toggleDrawer}
+                    isClose={true}
                 />
                 <div className="absolute z-50 left-1/2 transform -translate-x-1/2">
                     <Image src="/logo.svg" alt="Nay Logo" width={53} height={37} />
