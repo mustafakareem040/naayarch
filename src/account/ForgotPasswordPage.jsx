f39611817900f9f1214cd848a30b09c112ac9e8a
Mustafa Kareem <mus19pro@gmail.com>
2024-08-07 04:11:36 +0300

    Test 2

--- src/account/ForgotPasswordPage.jsx
+++ src/account/ForgotPasswordPage.jsx
@@ -1,4 +1,3 @@
-'use client'
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Link from "next/link";
@@ -19,7 +18,7 @@ const ForgotPasswordPage = () => {
     };
 
     return (
-            <div className="w-full mb-24 font-serif space-y-8">
+            <main className="w-full mb-24 font-serif space-y-8">
                 <div className="text-left">
                     <Link href="/login" className="flex mt-6 w-full items-center text-[#44594A] hover:text-[#374c3d]">
                         <Image src="/arrow-left.svg" alt={"arrow-left"} width={40} height={40}/>
@@ -58,7 +57,7 @@ const ForgotPasswordPage = () => {
                             {loading ? 'Processing...' : 'Reset Password'}
                         </button>
                 </form>
-            </div>
+            </main>
     );
 };
 
