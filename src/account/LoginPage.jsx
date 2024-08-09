b49cbd86bc4912de1fac8381968852cf86fc4296
Mustafa Kareem <mus19pro@gmail.com>
2024-08-10 02:23:46 +0300

    test

--- src/account/LoginPage.jsx
+++ src/account/LoginPage.jsx
@@ -29,7 +29,7 @@ export default function LoginPage() {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ email, password, rememberMe }),
-                    credentials: 'include' // This is important for sending and receiving cookies
+                    credentials: 'omit' // This is important for sending and receiving cookies
                 }
             );
 
