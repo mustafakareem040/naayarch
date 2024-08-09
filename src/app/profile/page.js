44535216ef805adbdf6a17e767bea52130e8d181
Mustafa Kareem <mus19pro@gmail.com>
2024-08-10 00:28:12 +0300

    Fixed profile maximum recursion

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -3,8 +3,9 @@ import LoginPage from "@/account/LoginPage";
 import {NotificationProvider} from "@/components/NotificationContext";
 import {NavBar} from "@/components/NavBar";
 import Footer from "@/components/Footer";
+import {Profile} from "@/components/Profile";
 
-export default function Profile() {
+export default function ProfilePage() {
     return (
         <>
             <NavBar />
