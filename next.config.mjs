b3720c6a6dba2febf20d4a2a69edb8130630e722
Mustafa <mus19pro@gmail.com>
2024-08-19 17:22:12 +0300

    Final commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 const nextConfig = {
-}
-if (process.env.NODE_ENV === 'development') {
-    await setupDevPlatform();
+    experimental: {
+        reactCompiler: true,
+    },
 }
 
 export default nextConfig;
