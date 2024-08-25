a69067dcd5d30b7dc493b200c5ef7c59fe01cca4
Mustafa Kareem <mus19pro@gmail.com>
2024-08-25 04:30:34 +0300

    est

--- src/app/profile/address/add/page.js
+++ src/app/profile/address/add/page.js
@@ -1,7 +1,6 @@
-import dynamic from "next/dynamic";
-const DynamicMap = dynamic(() => import("@/components/AddAddress"), { ssr: false });
 export default function AddAddressPage() {
     return (
-        <DynamicMap />
+        <></>
+        // <DynamicMap />
     )
 }
