64896b31ded493a56421d36aea0565737edc655f
Mustafa Kareem <mus19pro@gmail.com>
2024-08-27 02:13:37 +0300

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,7 @@
 import Dashboard from "@/components/Dashboard";
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import SearchComponent from "@/components/SearchComponent";
-import Products from "@/components/Products";
-import AsyncProducts from "@/components/AsyncProducts";
-import ProductDetail from "@/components/ProductDetail";
 
 export default function Home() {
     return (
-        // <Dashboard />
-        <ProductDetail title={"Test"} sizes={["lg", "xl"]} colors={["red", "green", "blue"]}
-                       price={"25000"} description={"test"} images={["/face.png", "/car.png", "/cream.png"]} />
+        <Dashboard />
   );
 }
