fddd75cff9ffafd54f8a3d31ca9b97d5f4e60dd1
Mustafa <mus19pro@gmail.com>
2024-08-29 15:41:28 +0300

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        reactCompiler: true,
-        ppr: true
+        reactCompiler: true
     },
     images: {
         remotePatterns: [
