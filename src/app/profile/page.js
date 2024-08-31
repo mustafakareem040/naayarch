c30975d0a5c992baf69b4be6b9bd959a66a9e0a7
Mustafa Kareem <mus19pro@gmail.com>
2024-08-31 20:10:56 +0300

    gray

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -9,13 +9,13 @@ export default function ProfilePage() {
     return (
         <>
             <AsyncNavBar bg={"#F6F3F1"} />
-            <div className="relative left-0 -m-4 right-0 min-h-[419px] top-0 -translate-y-[30%] -mb-[50%] w-[100vw]">
+            <div className="relative left-0 -translate-y-[10%] -m-4 right-0 min-h-[100vw] top-0 -mb-[25%] w-[100vw]">
                 <div className="font-sans absolute top-1/2 z-10 left-0 right-0 text-center text-3xl">
                     <p>Hi Mustafa!</p>
                     <p className="text-base font-serif">Let your beauty shine!</p>
                 </div>
             <Image  src={"/bg_flowers.png"} alt={"bg_flowers"} fill={true}
-            className=" object-cover"/>
+            className="object-contain"/>
             </div>
             <div className="h-screen flex flex-col justify-between">
                 <Profile />
