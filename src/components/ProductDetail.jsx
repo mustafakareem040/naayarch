424b39a70cde43e8cac00e20e9b04f695c6a707e
Mustafa <mus19pro@gmail.com>
2024-09-02 16:43:45 +0300

    Test

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -65,7 +65,7 @@ export default function ProductDetail({
                             alt={`Product image ${index + 1}`}
                             fill={true}
                             unoptimized={true}
-                            className="w-full object-cover h-[50vh]"
+                            className="w-full object-cover"
                             priority={index === 0}
                         />
                     </div>
