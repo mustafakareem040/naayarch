7cd897b542ced8100fb393170488301c0ca50411
Mustafa <mus19pro@gmail.com>
2024-09-04 17:14:52 +0300

    test

--- src/components/ProductLoading.jsx
+++ src/components/ProductLoading.jsx
@@ -4,6 +4,13 @@ import React from "react";
 
 export default function ProductLoading() {
     return (
-        <Image src={"/products-loading.gif"} loading={"eager"} priority={true} width={200} height={200} className="m-auto text-center"  alt={"Loading"}/>
+        <Image src={"/products-loading.gif"}
+               loading={"eager"}
+               priority={true}
+               placeholder={"blur"}
+               width={200}
+               height={200}
+               className="m-auto text-center"
+               alt={"Loading"}/>
     )
 }
