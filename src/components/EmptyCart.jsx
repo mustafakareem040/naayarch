6080fed1906340c664064a080023f15b1aa28ce1
Mustafa Kareem <mus19pro@gmail.com>
2024-09-06 12:57:54 +0300

    empty cart

--- src/components/EmptyCart.jsx
+++ src/components/EmptyCart.jsx
@@ -19,7 +19,7 @@ const EmptyCart = () => {
             </header>
 
             <main className="flex-grow flex flex-col items-center">
-                <div className="relative w-full aspect-[381/391] h-fit mb-4">
+                <div className="relative w-full h-[50vh]">
                     <Image
                         src="/empty.png"
                         alt="Empty cart"
