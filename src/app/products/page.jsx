2476fb5d5cfd4b7653d445a1fd986cb43847b5a6
Mustafa Kareem <mus19pro@gmail.com>
2024-09-14 21:45:15 +0300

    test

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -5,13 +5,25 @@ import ProductList from '@/components/ProductList';
 import AsyncNavBar from '@/components/AsyncNavBar';
 
 export const revalidate = 84600; // Revalidate every day
-
+export const runtime = "edge"
 const ITEMS_PER_PAGE = 15;
-
-async function fetchProducts(page = 1, query = '', c = '', sc = '') {
+async function fetchSubBrands() {
+    const response = await fetch("https://api.naayiq.com/subcategories/brands",
+        {
+            headers: { "Content-Type": 'application/json'},
+            next: {
+                revalidate: 84600
+            }
+        })
+    if (response.status === 200) {
+        return await response.json()
+    }
+    throw new Error(response.statusText);
+}
+async function fetchProducts(page = 1, query = '', c = '', sc = '', b = '') {
     const response = await fetch('https://api.naayiq.com/products', {
         headers: {
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/json'
         },
         next: {
             revalidate: 84600
@@ -21,7 +33,11 @@ async function fetchProducts(page = 1, query = '', c = '', sc = '') {
     if (!response.ok) {
         throw new Error('Failed to fetch products');
     }
-
+    let subCategories;
+    if (b) {
+        subCategories = await fetchSubBrands();
+        subCategories = subCategories.filter(item => item.category_id.toString() === b).map(item => item.id.toString());
+    }
     let { products } = await response.json();
 
     // Create filter function once
@@ -29,7 +45,10 @@ async function fetchProducts(page = 1, query = '', c = '', sc = '') {
         if (c && !product.categories.some(cat => cat.main_category_id?.toString() === c)) {
             return false;
         }
-        if (sc && !product.categories.some(cat => cat.id?.toString() === sc)) {
+        if (sc && product?.brand_id.toString() !== sc && !product.categories.some(cat => cat.id?.toString() === sc)) {
+            return false;
+        }
+        if (b && !subCategories.some(b_id => b_id === product?.brand_id.toString())) {
             return false;
         }
         if (query) {
@@ -68,8 +87,9 @@ async function ProductsPage({ searchParams }) {
     const query = searchParams.query || '';
     const c = searchParams.c || '';
     const sc = searchParams.sc || '';
+    const b = searchParams.b || '';
 
-    const { products, totalProducts, hasMore } = await fetchProducts(page, query, c, sc);
+    const { products, totalProducts, hasMore } = await fetchProducts(page, query, c, sc, b);
 
     return (
         <>
