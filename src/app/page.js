50f972252310b3d851929c71b23efa104820ffd2
Mustafa Kareem <mus19pro@gmail.com>
2024-09-15 14:24:23 +0300

    disabled ppr

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 import Dashboard from "@/components/Dashboard";
-export const experimental_ppr = true
 export const dynamic = 'force-static'
 export const dynamicParams = false
 export default function Home() {
