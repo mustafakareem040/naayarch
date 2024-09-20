2755eb86bcbbe4f351e3581e9e42b153e6f3416d
Mustafa Kareem <mus19pro@gmail.com>
2024-09-21 01:26:28 +0300

    test

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,32 +1,33 @@
-import './globals.css';
-import localFont from 'next/font/local';
-import { Figtree } from 'next/font/google';
+// app/layout.js
+import "./globals.css";
+import localFont from "next/font/local";
+import { Figtree } from "next/font/google";
 import NextTopLoader from 'nextjs-toploader';
-import Head from 'next/head';
-import React from 'react';
-import IsAuth from "@/components/isAuth";
-
-export const figtree = Figtree({ subsets: ['latin'], variable: '--fig', display: 'swap' });
+import StoreProvider from "@/app/StoreProvider";
+import Head from "next/head";
+import React from "react";
+import IsAuth from "@/components/isAuth"
+export const figtree = Figtree({ subsets: ["latin"], variable: "--fig", display: "swap" });
 const nnFont = localFont({
     src: [
         {
-            path: './fonts/nn-regular.woff2',
-            weight: '400',
-            style: 'normal',
+            path: "./fonts/nn-regular.woff2",
+            weight: "400",
+            style: "normal",
         },
         {
-            path: './fonts/nn-medium.woff2',
-            weight: '500',
-            style: 'normal',
+            path: "./fonts/nn-medium.woff2",
+            weight: "500",
+            style: "normal",
         },
     ],
-    variable: '--nn-font',
-    display: 'swap',
+    variable: "--nn-font",
+    display: "swap"
 });
 
 export const metadata = {
-    title: 'NaayIraq',
-    description: 'An Iraqi store for skin care products',
+    title: "NaayIraq",
+    description: "An Iraqi store for skin care products",
 };
 
 export default function RootLayout({ children }) {
@@ -34,7 +35,7 @@ export default function RootLayout({ children }) {
         <html lang="en">
         <Head>
             <title>NaayIraq</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
         </Head>
         <body className={`m-4 box-border overflow-x-hidden ${nnFont.variable} ${figtree.variable}`}>
         <NextTopLoader
@@ -47,8 +48,10 @@ export default function RootLayout({ children }) {
             speed={300}
             shadow="0 0 10px #2299DD,0 0 5px #2299DD"
         />
-        <IsAuth />
-        {children}
+        <StoreProvider>
+            <IsAuth />
+            {children}
+        </StoreProvider>
         </body>
         </html>
     );
