44285d790702de0abba03ba5b37b2756d303c1af
Mustafa Kareem <mus19pro@gmail.com>
2024-09-21 21:36:50 +0300

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        reactCompiler: true
+        reactCompiler: true,
+        optimizeCss: true,
+        optimisticClientCache: true,
+        webpackMemoryOptimizations: true,
+        parallelServerCompiles: true
+    },
+    webpack: (config, { dev, isServer }) => {
+        if (!dev && !isServer) {
+            Object.assign(config.resolve.alias, {
+                react: 'preact/compat',
+                'react-dom/test-utils': 'preact/test-utils',
+                'react-dom': 'preact/compat',
+            });
+        }
+
+        return config;
     },
     images: {
         remotePatterns: [
