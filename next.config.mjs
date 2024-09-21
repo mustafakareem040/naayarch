38613bc739ddc706e9a15c61eab6e20ab17e01e9
Mustafa Kareem <mus19pro@gmail.com>
2024-09-21 21:39:55 +0300

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        reactCompiler: true,
         optimizeCss: true,
         optimisticClientCache: true,
         webpackMemoryOptimizations: true,
