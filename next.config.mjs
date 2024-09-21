c0ead98bf90e74ffd4f52f7ef7bcdedd7d48bb89
Mustafa Kareem <mus19pro@gmail.com>
2024-09-21 21:44:10 +0300

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config, { dev, isServer }) => {
-        if (!dev && !isServer) {
-            Object.assign(config.resolve.alias, {
-                react: 'preact/compat',
-                'react-dom/test-utils': 'preact/test-utils',
-                'react-dom': 'preact/compat',
-            });
-        }
-
-        return config;
+    experimental: {
+        reactCompiler: true
     },
     images: {
         remotePatterns: [
