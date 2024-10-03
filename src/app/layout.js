3f09228cbef52b5c0ef4d7454aad82a2cf5aab40
Mustafa Kareem <mus19pro@gmail.com>
2024-10-03 21:12:39 +0300

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,6 +24,34 @@ const nnFont = localFont({
     display: "swap"
 });
 
+export const metadata = {
+    title: {
+        default: "Naay - Your Iraqi Store for Korean and Global Beauty Products",
+        template: "%s | Naay"
+    },
+    description: "Discover premium Korean and global beauty products at Naay, Iraq's leading online store for skincare, makeup, and body care.",
+    keywords: ["Iraqi beauty store", "Korean skincare", "Iraqi skincare store", "Naay Iraq", "NaayIraq", "naayiq", "Naay", "Global beauty brands", "Makeup", "Skincare", "Body care"],
+    openGraph: {
+        title: "Naay - Premium Beauty Products in Iraq",
+        description: "Shop the best Korean and global beauty products at Naay. Your one-stop shop for skincare, makeup, and more in Iraq.",
+        url: "https://naayiq.com",
+        siteName: "Naay",
+        locale: "en_US",
+        type: "website",
+    },
+    robots: {
+        index: true,
+        follow: true,
+        googleBot: {
+            index: true,
+            follow: true,
+            "max-video-preview": -1,
+            "max-image-preview": "large",
+            "max-snippet": -1,
+        },
+    }
+};
+
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
