a217c34d60c6893940d58c249162fbe2f0895a38
Mustafa Kareem <mus19pro@gmail.com>
2024-10-16 04:07:19 +0300

    tset

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -67,14 +67,14 @@ const ProductItem = memo(({ id, name, price, imageUrl, product, handleClick, onC
 
                 {!imageLoaded && <ImageSkeleton />}
                 <Image
-                    src={`https://storage.naayiq.com/resources/${removeFileExtension(imageUrl)}_optimized.webp`}
+                    src={`https://storage.naayiq.com/resources/${imageUrl}`}
                     alt={name}
-                    fill={true}
-                    unoptimized={true}
-                    sizes="(max-width: 768px) 50vw, (max-width: 1200px) 25vw, 10vw"
                     onError={(e) => {
                         e.target.src = `https://storage.naayiq.com/resources/${imageUrl}`;
                     }}
+                    width={300}
+                    height={300}
+                    quality={90}
                     onLoad={handleImageLoad}
                     className={`object-cover relative rounded-t-lg w-full h-full overflow-hidden transition-transform duration-300 group-hover:scale-110 ${imageLoaded ? 'opacity-100' : 'opacity-0'}`}
                 />
